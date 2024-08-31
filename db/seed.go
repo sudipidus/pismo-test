8d3f6cc08710ba00415e74167f4167cf2ee9a230
Sudip Bhandari <sudip.post@gmail.com>
2024-08-31 12:06:50 +0530

    uncommented seed

--- db/seed.go
+++ db/seed.go
@@ -1,11 +1,14 @@
 package db
 
 import (
+	"context"
 	//TODO: perhaps migrate belongs better in storage
 	_ "github.com/lib/pq"
 	"github.com/sudipidus/pismo-test/logger"
 	"github.com/sudipidus/pismo-test/models"
 	storage2 "github.com/sudipidus/pismo-test/storage"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func SeedOperationType(storage2 storage2.Storage) {
@@ -35,10 +38,10 @@ func SeedOperationType(storage2 storage2.Storage) {
 			IsCredit:    false,
 		},
 	}
-	//_, err := storage2.SeedOperationType(context.Background(), opTypes)
-	//if err != nil {
-	//	logger.GetLogger().Fatal("Seeding of operation type data failed.. aborting", zap.Field{Key: "error", Type: zapcore.ErrorType, Interface: err})
-	//}
+	_, err := storage2.SeedOperationType(context.Background(), opTypes)
+	if err != nil {
+		logger.GetLogger().Fatal("Seeding of operation type data failed.. aborting", zap.Field{Key: "error", Type: zapcore.ErrorType, Interface: err})
+	}
 	_ = opTypes
 	logger.GetLogger().Info("Seeding of operation type data complete")
 
